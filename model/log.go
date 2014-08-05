da95ac4c8cce80e77ae49dc65be72831222cc45a
Chen Liang <chen.liang@crenolab.com>
2014-08-05 09:31:05 +0000

    rename namespace

--- model/log.go
+++ model/log.go
@@ -36,11 +36,11 @@ var LevelMapping = map[string]LEVEL{
 }
 
 type Log struct {
-	Id        int       `xorm:"int pk autoincr 'id'"`
+	Id        int       `xorm:"int"`
 	App       int64     `xorm:"int not null 'app'"`
 	Level     string    `xorm:"text not null 'level'"`
 	Action    string    `xorm:"text not null 'action'"`
-	FromModel string    `xorm:"text not null 'from_model'"`
+	Module    string    `xorm:"text not null 'module'"`
 	Log       string    `xorm:"text not null 'log'"`
 	ParentLog string    `xorm:"text 'parentLog'"`
 	Created   time.Time `xorm:"created"`
@@ -78,35 +78,34 @@ func InsertLog(logJson []byte) {
 	return
 }
 
-func WriteLog(logChanP *chan string, sysLogLevel string, logLevel string, app int64, action string, fromModel string, logId string) string {
+func WriteLog(logChan *chan string, sysLogLevel string, logLevel string, app int64, action string, module string, logId string) string {
 	if LevelMapping[sysLogLevel] >= LevelMapping[logLevel] {
 		parentLogId := logId
-		md5String := fmt.Sprintf("%v%v%v", logId, fromModel, string(time.Now().Unix()))
+		md5String := fmt.Sprintf("%v%v%v", logId, module, string(time.Now().Unix()))
 		h := md5.New()
 		h.Write([]byte(md5String))
 		logId = hex.EncodeToString(h.Sum(nil))
 
-		logModel := new(Log)
-		logModel.App = app
-		logModel.Level = sysLogLevel
-		logModel.Action = action
-		logModel.FromModel = fromModel
-		logModel.Log = logId
-		logModel.ParentLog = parentLogId
-		logJson, _ := json.Marshal(logModel)
-		*logChanP <- string(logJson)
+		log := new(Log)
+		log.App = app
+		log.Level = sysLogLevel
+		log.Action = action
+		log.Module = module
+		log.Log = logId
+		log.ParentLog = parentLogId
+		logJson, _ := json.Marshal(log)
+		*logChan <- string(logJson)
 		return logId
 	}
 	return ""
 }
 
-func Sendlog(logChanP *chan string) {
+func Sendlog(logChan *chan string) {
 	for {
-		log_data := <-(*logChanP)
-		var logModel Log
-		json.Unmarshal([]byte(log_data), &logModel)
+		log_data := <-(*logChan)
+		var log Log
+		json.Unmarshal([]byte(log_data), &log)
 		request, _ := http.NewRequest("POST", "http://log.arkors.com/v1/log", bytes.NewReader([]byte(log_data)))
-		request.Header.Set("X-Arkors-Application-Log", logModel.Log)
 		request.Header.Set("X-Arkors-Application-Client", "127.0.0.1,OAUTH")
 		request.Header.Set("Accept", "application/json")
 		client := &http.Client{}
